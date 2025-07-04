bd79cabf85c4f5d8cee0af0cb8eb7df67d368da0
NICOLÁS ALBERTO ESCOBAR QUINTRIQUEO <nescobarq@alumnos.ceduc.cl>
2025-07-04 15:23:29 -0400

    arreglo de stock

--- backend/bodega/views.py
+++ backend/bodega/views.py
@@ -4,11 +4,17 @@ from .models import Producto
 from .serializers import ProductoSerializer
 
 class ProductoViewSet(viewsets.ModelViewSet):
-    queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
     filter_backends = [filters.SearchFilter, DjangoFilterBackend, filters.OrderingFilter]
-    search_fields = ['nombre', 'codigo']  # Habilita la búsqueda con ?search=
+    search_fields = ['nombre', 'codigo']
     filterset_fields = ['nombre', 'codigo']
     ordering_fields = ['nombre', 'codigo']
     ordering = ['codigo']
-    pagination_class = None  
+    pagination_class = None  # Desactiva paginación
+
+    def get_queryset(self):
+        queryset = Producto.objects.all().order_by('codigo')
+        search = self.request.query_params.get('search')
+        if search:
+            return queryset.filter(nombre__icontains=search)[:25]
+        return queryset[:20]
